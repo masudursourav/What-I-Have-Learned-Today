f9ca5132d4e1a9a9a5f113c2b225042e43602f91
masudursourav <masudursourav@ieee.org>
2023-07-20 21:09:31 +0600

    added categorey options

--- src/components/NewFactForm.jsx
+++ src/components/NewFactForm.jsx
@@ -1,4 +1,17 @@
+/* eslint-disable react/jsx-key */
 import "../style.css"
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 const NewFactForm = () => {
     return (
         <>
@@ -8,9 +21,7 @@ const NewFactForm = () => {
         <input type="text" placeholder="Trustworthy source..." />
         <select>
           <option value="">Choose category:</option>
-          <option value="technology">Technology</option>
-          <option value="science">Science</option>
-          <option value="finance">Finance</option>
+          {CATEGORIES.map(cat => <option value={cat.name}>{cat.name.toLocaleUpperCase()}</option>)}
         </select>
         <button className="btn btn-large">Post</button>
       </form>   
